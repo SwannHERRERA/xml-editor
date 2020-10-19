3449d5598c00a53378efdfd6ba2bc8184a669b06
pooetitu <pooetitu@gmail.com>
2020-10-19 20:53:19 +0200

    Adding basic element structure and functions

--- main.c
+++ main.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct XMLElement XMLElement;
 int find_doctype(FILE *file);
 long get_size_of_file(FILE *file);
 char *file_get_content(FILE *file);
+void add_element(XMLElement* parent, XMLElement* child);
+XMLElement* create_element(XMLElement* parent, int deepness);
+
+struct XMLElement{
+  XMLElement* parent;
+  XMLElement** childs;
+  int deepness;
+  int childsCount;
+  int childsCapacity;
+};
+
 
 /**
  * I use ARGV to give relative path to xml
@@ -60,3 +72,29 @@ long get_size_of_file(FILE *file)
   fseek(file, current_size, SEEK_CUR);
   return size;
 }
+
+void add_element(XMLElement* parent, XMLElement* child){
+  if(parent->childsCount == parent->childsCapacity){
+    parent->childsCapacity *= 2;
+    XMLElement** tab = malloc(sizeof(XMLElement)*parent->childsCapacity);
+    for(int i = 0; i < parent->childsCount;i++){
+      tab[i] = parent->childs[i];
+    }
+    parent->childs = tab;
+  }
+  parent->childsCount += 1;
+  parent->childs[parent->childsCount] = child;
+}
+
+XMLElement* create_element(XMLElement* parent, int deepness){
+  XMLElement* element = malloc(sizeof(XMLElement));
+  if(parent != NULL){
+    add_element(parent, element);
+  }
+  element->parent = parent;
+  element->deepness = deepness;
+  element->childsCount = 0;
+  element->childsCapacity = 1;
+  element->childs = malloc(sizeof(XMLElement));
+}
+
