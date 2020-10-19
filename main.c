a4fc2d0ac292ad60befce258cad4aa4c6ac42189
Swann <swann@devloup.dev>
2020-10-19 20:12:07 +0200

    first step, add utility function

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+int find_doctype(FILE *file);
+long get_size_of_file(FILE *file);
+char *file_get_content(FILE *file);
+
 /**
  * I use ARGV to give relative path to xml
- *
  */
 int main(int argc, char **argv)
 {
@@ -20,4 +23,40 @@ int main(int argc, char **argv)
     fprintf(stderr, "Error opening file %s\n", argv[1]);
     return EXIT_FAILURE;
   }
+  printf("%d\n", find_doctype(file));
+  fclose(file);
+}
+
+// A terme j'aimerais bien que ça retourn
+// <!DOCTYPE ESGI [<!ELEMENT classrooms (classroom+)><!ELEMENT classroom (#PCDATA)>]>
+int find_doctype(FILE *file)
+{
+  char *buffer = file_get_content(file);
+  if (strstr(buffer, "<!DOCTYPE") != NULL)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+char *file_get_content(FILE *file)
+{
+  long size = get_size_of_file(file);
+  char *buffer = malloc(sizeof(char) * size);
+  if (buffer)
+  {
+    fread(buffer, sizeof(char), size, file);
+  }
+  return buffer;
+}
+
+long get_size_of_file(FILE *file)
+{
+  long size;
+  long current_size = ftell(file);
+  fseek(file, 0L, SEEK_END);
+  size = ftell(file);
+  fseek(file, 0L, SEEK_SET);
+  fseek(file, current_size, SEEK_CUR);
+  return size;
 }
