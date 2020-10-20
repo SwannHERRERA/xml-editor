254c862c70664315ee5635670c2d218194aa407e
Swann <swann@devloup.dev>
2020-10-20 23:10:29 +0200

    make blueprint

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "parse_dtd.h"
 
+bool isCorrectXML(FILE *xml);
+
 /**
  * I use ARGV to give relative path to xml
  */
@@ -17,9 +19,22 @@ int main(int argc, char **argv)
     fprintf(stderr, "Error opening file %s\n", argv[1]);
     return EXIT_FAILURE;
   }
+  if (!isCorrectXML(file))
+  {
+    fprintf(stderr, "Error XML invalid");
+  }
   char *dtd = find_doctype(file);
   fclose(file);
   parse_dtd(dtd);
   free(dtd);
   return EXIT_SUCCESS;
+}
+
+bool isCorrectXML(FILE *xml)
+{
+  // Check 1 chevron ouvrant = 1 chevron fermant ⚠️ commentaire
+  // Check nombre pair de " non \\
+  // Il faut pas qu'il y ait de "<! "
+
+  return true;
 }
