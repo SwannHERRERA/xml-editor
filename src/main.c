6f7aeb79be225151cffece96682a0c936045abcf
Swann <swann@devloup.dev>
2020-10-27 23:44:26 +0100

    Parse XML start create attribute

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
 #include "parse_dtd.h"
 
+typedef struct xml_attribute
+{
+  char *name;
+  char *value;
+
+} xml_attribute;
+typedef struct xml_element
+{
+  char *name;
+  xml_attribute *attributes;
+  char *content;
+} xml_element;
+
+xml_element *get_root_balise(char *xml, char *root_name);
+
 /**
  * I use ARGV to give relative path to xml
  */
@@ -20,6 +35,16 @@ int main(int argc, char **argv)
   char *root_name;
   char *dtd = find_doctype(file, &root_name);
   fclose(file);
+
+  char *xml = "<classrooms attr='fefe' b='ef' c='' derrerzer='qs'>"
+              "< classroom > AL</ classroom>"
+              "<classroom> IABD</ classroom>"
+              "<classroom> MOC</ classroom>"
+              "<classroom> IBC</ classroom>"
+              "</ classrooms> ";
+
+  get_root_balise(xml, root_name);
+
   parse_dtd(dtd, root_name);
   free(dtd);
   free(root_name);
@@ -39,4 +64,64 @@ bool isWhiteSpaceCharacter(char c)
     }
   }
   return false;
+}
+
+xml_element *get_root_balise(char *xml, char *root_name)
+{
+  xml_element *res = malloc(sizeof(xml_element));
+  res->name = root_name;
+  // char *start_attr;
+  // char *end_attr;
+  char *start;
+  char *tag;
+  char *tmp = (char *)calloc(strlen(root_name) + 1, sizeof(char));
+  strcpy(tmp, "<");
+  start = strstr(xml, strcat(tmp, root_name));
+  tmp = start;
+  size_t i = 0;
+  size_t j = 0;
+  size_t counter;
+  // Care about <b attr=">">
+  while (tmp[i] != '>')
+  {
+    if (tmp[i] == '=')
+    {
+      xml_attribute *new_attrbute = malloc(sizeof(xml_attribute));
+      j = i;
+      counter = 0;
+      while (tmp[j] != ' ')
+      {
+        j -= 1;
+        counter += 1;
+      }
+      new_attrbute->name = calloc(counter, sizeof(char));
+      strncpy(new_attrbute->name, tmp + i - counter, counter);
+      printf("attr_name: %s\n", new_attrbute->name);
+      i += 2; // skip first "
+      j = i;
+      while (tmp[j] != '"' && tmp[j] != '\'' && j < strlen(tmp))
+      {
+        j += 1;
+      }
+      new_attrbute->value = calloc(j - i, sizeof(char));
+      strncpy(new_attrbute->value, tmp + i, j - i);
+      printf("attr_value: %s\n", new_attrbute->value);
+    }
+
+    if (i > strlen(start))
+    {
+      fprintf(stderr, "No '>' in xml \n");
+      exit(EXIT_FAILURE);
+    }
+    i += 1;
+  }
+  tag = malloc(sizeof(char) * (i + 2));
+  strncpy(tag, start, (i + 1) * sizeof(char));
+  printf("%s\n", tag);
+  return res;
+  // GET XML STRING
+  // <root_name ... >
+  // stack qui ouvre et qui ferme
+  // <root_name ... >
+  // ELEMENT name;
 }
