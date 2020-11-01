e216428ede2a5934789909f7bd698df5b3559eec
Swann <swann@devloup.dev>
2020-11-01 18:21:02 +0100

    get content, free and print

--- src/main.c
+++ src/main.c
@@ -85,7 +85,7 @@ xml_attribute_linkedlist *create_next_element(xml_attribute_linkedlist *head)
   return new_attribute;
 }
 
-void make_attributes(char *tag, char *subject, xml_element *element)
+int make_attributes(char *tag, char *subject, xml_element *element)
 {
   char *start;
   size_t i = 0, j = 0, counter;
@@ -134,6 +134,8 @@ void make_attributes(char *tag, char *subject, xml_element *element)
     i += 1;
   }
   element->attributes = head;
+  i += 1; // skip >
+  return i;
 }
 
 void create_empty_xml_attribute_linkedlist(xml_element *element)
@@ -156,14 +158,52 @@ void print_attribute(xml_element *element)
   }
 }
 
+void get_content(char *subject, xml_element *element)
+{
+  char *content = (char *)malloc(sizeof(char) * strlen(subject));
+  strcpy(content, subject);
+  char *end = strstr(content, "</classrooms>");
+  end[0] = '\0';
+  element->content = content;
+}
+
+void print_element(xml_element *element)
+{
+  printf("%s\n", element->name);
+  print_attribute(element);
+  printf("%s\n", element->content);
+}
+
+void free_element(xml_element *element)
+{
+  xml_attribute_linkedlist *tmp = NULL;
+  unsigned int i;
+
+  for (i = 0; i < element->number_of_attribute; i += 1)
+  {
+    free(element->attributes->value->name);
+    free(element->attributes->value->value);
+    free(element->attributes->value);
+    tmp = element->attributes;
+    element->attributes = element->attributes->next;
+    free(tmp);
+  }
+  free(element->name);
+  free(element->content);
+  free(element);
+}
+
 xml_element *get_root_balise(char *xml, char *root_name)
 {
+  int index_of_opening_tag;
   xml_element *root_tag = malloc(sizeof(xml_element));
   root_tag->name = root_name;
   root_tag->number_of_attribute = 0;
   create_empty_xml_attribute_linkedlist(root_tag);
-  make_attributes(root_name, xml, root_tag);
-  print_attribute(root_tag);
-
+  index_of_opening_tag = make_attributes(root_name, xml, root_tag);
+  char *start = xml + sizeof(char) * index_of_opening_tag;
+  get_content(start, root_tag);
+  print_element(root_tag);
+  free_element(root_tag);
   return root_tag;
 }
