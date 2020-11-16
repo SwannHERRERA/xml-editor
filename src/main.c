50bb45e276231facdc1ff6731de6941e482eb3ca
Swann <swann@devloup.dev>
2020-11-16 22:19:58 +0100

    remove check xml is corresponding to DTD

--- src/main.c
+++ src/main.c
@@ -2,8 +2,6 @@
 #include "parse_xml.h"
 #include "interface.h"
 
-bool check_xml_correspond_to_xml(XMLElement *dtd, xml_element *root);
-bool check_element_is_correct(XMLElement *dtd_element, xml_element *element);
 /**
  * I use ARGV to give relative path to xml
  */
@@ -34,15 +32,6 @@ int main(int argc, char **argv)
   print_element(root);
   printf("\n######## Finished PARSE XML ########\n");
 
-  if (check_xml_correspond_to_xml(dtd, root))
-  {
-    printf("XML is corresponding to DTD\n");
-  }
-  else
-  {
-    printf("XML is NOT corresponding to DTD\n");
-  }
-
   free_DTD(dtd);
   free_element(root);
   free(xml);
@@ -51,47 +40,4 @@ int main(int argc, char **argv)
   free(root_name);
   init_interface(&argc, &argv);
   return EXIT_SUCCESS;
-}
-
-bool check_xml_correspond_to_xml(XMLElement *dtd, xml_element *root)
-{
-  if (strcmp(dtd->name, root->name) == 0)
-  {
-    return check_element_is_correct(dtd, root);
-  }
-  return false;
-}
-bool check_element_is_correct(XMLElement *dtd_element, xml_element *element)
-{
-  int i, j;
-  int tab[dtd_element->childsCount];
-
-  for (i = 0; i < dtd_element->childsCount; i += 1)
-  {
-    tab[i] = 0;
-  }
-
-  for (i = 0; i < element->childs_count; i += 1)
-  {
-    for (j = 0; j < dtd_element->childsCount; j += 1)
-    {
-      if (strcmp(element->childs[i]->name, dtd_element->childs[j]->name) == 0)
-      {
-        tab[j] += 1;
-        if (check_element_is_correct(dtd_element->childs[j], element->childs[i]) == false)
-        {
-          return false;
-        }
-      }
-    }
-  }
-
-  // check les attributes
-
-  // check with occurenceFlag
-  for (i = 0; i < dtd_element->childsCount; i += 1)
-  {
-    printf("%s: %d\n", dtd_element->childs[i]->name, tab[i]);
-  }
-  return true;
 }
