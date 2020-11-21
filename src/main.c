17e50b435fc1e36358c4f149cd413fe14cc4d4ca
Swann <swann@devloup.dev>
2020-11-21 21:47:23 +0100

    fix seg fault on error in inputFIle param

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@ int main(int argc, char **argv)
   FILE *file = fopen(argv[1], "r");
   if (file == NULL)
   {
-    fclose(file);
     fprintf(stderr, "Error opening file %s\n", argv[1]);
     return EXIT_FAILURE;
   }
