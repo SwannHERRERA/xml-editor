bace7cd8c96cf899830f289b47d39082b6133214
pooetitu <pooetitu@gmail.com>
2020-11-25 00:05:29 +0100

    added new file

--- src/interface.h
+++ src/interface.h
@@ -40,7 +40,7 @@ void menu_button_about();
 void menu_button_save_file_as(GtkWidget *widget, gpointer data);
 void menu_button_save_file();
 void menu_button_open_file(GtkWidget *widget, gpointer data);
-void menu_button_new_file();
+void menu_button_new_file(GtkWidget *widget, gpointer data);
 void text_view_populate_popup_menu(GtkWidget *widget, GtkWidget *popup, gpointer data);
 void start_xml_validation(GtkWidget *widget, gpointer data);
 #endif
