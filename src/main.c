e026fcc833f53380cede4538521ec70d7f18f981
Swann <swann@devloup.dev>
2020-11-25 23:49:20 +0100

    remove main

--- src/main.c
+++ src/main.c
@@ -3,47 +3,6 @@
 
 int main(int argc, char **argv)
 {
-  
-  if (argc < 2)
-  {
-    fprintf(stderr, "Error attending xml file in parameters\n");
-    return EXIT_FAILURE;
-  }
-  FILE *file = fopen(argv[1], "r");
-  if (file == NULL)
-  {
-    fprintf(stderr, "Error opening file %s\n", argv[1]);
-    return EXIT_FAILURE;
-  }
-  char *xml_file_content = file_get_content(file);
-  char *root_name = NULL;
-  char *dtd_string = find_doctype(xml_file_content, &root_name);
-  DTD_element *dtd = parse_dtd(dtd_string, root_name);
-
-  printf("\n######## Starting PARSE XML ########\n");
-  fseek(file, 0L, SEEK_SET);
-  XML_element *root = parse_xml(xml_file_content);
-  print_element(root);
-  printf("\n######## Finished PARSE XML ########\n");
-
-  if (check_dtd_correspond_to_xml(dtd, root))
-  {
-    printf("XML is corresponding to DTD\n");
-  }
-  else
-  {
-    printf("XML is NOT corresponding to DTD\n");
-  }
-
-  if (dtd != NULL)
-  {
-    free_DTD(dtd);
-  }
-  free_element(root);
-  free(xml_file_content);
-  fclose(file);
-  free(dtd_string);
-  free(root_name);
   init_interface(&argc, &argv);
   return EXIT_SUCCESS;
 }
